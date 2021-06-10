547b90d9ad8bb185881813aafda583162cb20ff6
Marina <marinabrito.ma@gmail.com>
2021-06-10 13:49:20 +0100

    added: axios, api, promisse, catch

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 $(document).ready(() => {
     $('#searchForm').on('submit', (e) => {
-        console.log($('#searchText').val());
+        console.log($('#searchText').val())
+        let searchText = $('#searchText').val();
+        getMovies(searchText);
         e.preventDefault();
     });
 });
+
+function getMovies(searchText) {
+    console.log(searchText)
+    axios.get('http://www.omdbapi.com?&apikey=thewdb&s='+searchText)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
